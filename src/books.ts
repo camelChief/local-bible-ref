b5700c47dc4023a614f55a16635b3d2cf41b5d5c
Caleb Campbell <ccampbell@biscit.com.au>
2025-12-20 14:26:28 +1100

    7 - Added extra abbreviations to books.

--- src/books.ts
+++ src/books.ts
@@ -5,69 +5,240 @@ export interface Book {
 
 export const BOOKS: Book[] = [
     { name: "Genesis", aliases: ["gen"] },
-    { name: "Exodus", aliases: ["exo"] },
+    { name: "Exodus", aliases: ["exo", "ex", "exod"] },
     { name: "Leviticus", aliases: ["lev"] },
     { name: "Numbers", aliases: ["num"] },
-    { name: "Deuteronomy", aliases: ["deu"] },
-    { name: "Joshua", aliases: ["jos"] },
-    { name: "Judges", aliases: ["jdg"] },
+    { name: "Deuteronomy", aliases: ["deu", "deut"] },
+    { name: "Joshua", aliases: ["jos", "josh"] },
+    { name: "Judges", aliases: ["jdg", "judg"] },
     { name: "Ruth", aliases: ["rut"] },
-    { name: "1 Samuel", aliases: ["1sa", "1samuel"] },
-    { name: "2 Samuel", aliases: ["2sa", "2samuel"] },
-    { name: "1 Kings", aliases: ["1ki", "1kings"] },
-    { name: "2 Kings", aliases: ["2ki", "2kings"] },
-    { name: "1 Chronicles", aliases: ["1ch", "1chronicles"] },
-    { name: "2 Chronicles", aliases: ["2ch", "2chronicles"] },
+    {
+        name: "1 Samuel",
+        aliases: [
+            "1sa",
+            "1samuel",
+            "1 sam",
+            "1st sam",
+            "1st samuel",
+            "first samuel"
+        ]
+    },
+    {
+        name: "2 Samuel",
+        aliases: [
+            "2sa",
+            "2samuel",
+            "2 sam",
+            "2nd sam",
+            "2nd samuel",
+            "second samuel"
+        ]
+    },
+    {
+        name: "1 Kings",
+        aliases: [
+            "1ki",
+            "1kings",
+            "1 kgs",
+            "1st kgs",
+            "1st kings",
+            "first kings"
+        ]
+    },
+    {
+        name: "2 Kings",
+        aliases: [
+            "2ki",
+            "2kings",
+            "2 kgs",
+            "2nd kgs",
+            "2nd kings",
+            "second kings"
+        ]
+    },
+    {
+        name: "1 Chronicles",
+        aliases: [
+            "1ch",
+            "1chronicles",
+            "1 chr",
+            "1st chr",
+            "1st chronicles",
+            "first chronicles"
+        ]
+    },
+    {
+        name: "2 Chronicles",
+        aliases: [
+            "2ch",
+            "2chronicles",
+            "2 chr",
+            "2nd chr",
+            "2nd chronicles",
+            "second chronicles"
+        ]
+    },
     { name: "Ezra", aliases: ["ezr"] },
     { name: "Nehemiah", aliases: ["neh"] },
-    { name: "Esther", aliases: ["est"] },
+    { name: "Esther", aliases: ["est", "esth"] },
     { name: "Job", aliases: ["job"] },
-    { name: "Psalms", aliases: ["psa"] },
-    { name: "Proverbs", aliases: ["pro"] },
-    { name: "Ecclesiastes", aliases: ["ecc"] },
-    { name: "Song of Songs", aliases: ["sng", "Song of Solomon"] },
+    { name: "Psalms", aliases: ["psa", "ps", "psalm"] },
+    { name: "Proverbs", aliases: ["pro", "prov"] },
+    { name: "Ecclesiastes", aliases: ["ecc", "eccl"] },
+    {
+        name: "Song of Songs",
+        aliases: [
+            "sng",
+            "Song of Solomon",
+            "song"
+        ]
+    },
     { name: "Isaiah", aliases: ["isa"] },
     { name: "Jeremiah", aliases: ["jer"] },
     { name: "Lamentations", aliases: ["lam"] },
-    { name: "Ezekiel", aliases: ["ezk"] },
+    { name: "Ezekiel", aliases: ["ezk", "ezek"] },
     { name: "Daniel", aliases: ["dan"] },
     { name: "Hosea", aliases: ["hos"] },
     { name: "Joel", aliases: ["jol"] },
     { name: "Amos", aliases: ["amo"] },
-    { name: "Obadiah", aliases: ["oba"] },
+    { name: "Obadiah", aliases: ["oba", "obad"] },
     { name: "Jonah", aliases: ["jon"] },
     { name: "Micah", aliases: ["mic"] },
     { name: "Nahum", aliases: ["nam"] },
     { name: "Habakkuk", aliases: ["hab"] },
-    { name: "Zephaniah", aliases: ["zep"] },
+    { name: "Zephaniah", aliases: ["zep", "zeph"] },
     { name: "Haggai", aliases: ["hag"] },
-    { name: "Zechariah", aliases: ["zec"] },
+    { name: "Zechariah", aliases: ["zec", "zech"] },
     { name: "Malachi", aliases: ["mal"] },
-    { name: "Matthew", aliases: ["mat"] },
+    { name: "Matthew", aliases: ["mat", "matt"] },
     { name: "Mark", aliases: ["mrk"] },
     { name: "Luke", aliases: ["luk"] },
     { name: "John", aliases: ["jhn"] },
     { name: "Acts", aliases: ["act"] },
     { name: "Romans", aliases: ["rom"] },
-    { name: "1 Corinthians", aliases: ["1co", "1corinthians"] },
-    { name: "2 Corinthians", aliases: ["2co", "2corinthians"] },
+    {
+        name: "1 Corinthians",
+        aliases: [
+            "1co",
+            "1corinthians",
+            "1 cor",
+            "1st cor",
+            "1st corinthians",
+            "first corinthians"
+        ]
+    },
+    {
+        name: "2 Corinthians",
+        aliases: [
+            "2co",
+            "2corinthians",
+            "2 cor",
+            "2nd cor",
+            "2nd corinthians",
+            "second corinthians"
+        ]
+    },
     { name: "Galatians", aliases: ["gal"] },
     { name: "Ephesians", aliases: ["eph"] },
-    { name: "Philippians", aliases: ["php"] },
+    { name: "Philippians", aliases: ["php", "phil"] },
     { name: "Colossians", aliases: ["col"] },
-    { name: "1 Thessalonians", aliases: ["1th", "1thessalonians"] },
-    { name: "2 Thessalonians", aliases: ["2th", "2thessalonians"] },
-    { name: "1 Timothy", aliases: ["1ti", "1timothy"] },
-    { name: "2 Timothy", aliases: ["2ti", "2timothy"] },
+    {
+        name: "1 Thessalonians",
+        aliases: [
+            "1th",
+            "1thessalonians",
+            "1 thess",
+            "1st thess",
+            "1st thessalonians",
+            "first thessalonians"
+        ]
+    },
+    {
+        name: "2 Thessalonians",
+        aliases: [
+            "2th",
+            "2thessalonians",
+            "2 thess",
+            "2nd thess",
+            "2nd thessalonians",
+            "second thessalonians"
+        ]
+    },
+    {
+        name: "1 Timothy",
+        aliases: [
+            "1ti",
+            "1timothy",
+            "1 tim",
+            "1st tim",
+            "1st timothy",
+            "first timothy"
+        ]
+    },
+    {
+        name: "2 Timothy",
+        aliases: [
+            "2ti",
+            "2timothy",
+            "2 tim",
+            "2nd tim",
+            "2nd timothy",
+            "second timothy"
+        ]
+    },
     { name: "Titus", aliases: ["tit"] },
-    { name: "Philemon", aliases: ["phm"] },
+    { name: "Philemon", aliases: ["phm", "phlm"] },
     { name: "Hebrews", aliases: ["heb"] },
     { name: "James", aliases: ["jas"] },
-    { name: "1 Peter", aliases: ["1pe", "1peter"] },
-    { name: "2 Peter", aliases: ["2pe", "2peter"] },
-    { name: "1 John", aliases: ["1jn", "1john"] },
-    { name: "2 John", aliases: ["2jn", "2john"] },
-    { name: "3 John", aliases: ["3jn", "3john"] },
+    {
+        name: "1 Peter",
+        aliases: [
+            "1pe",
+            "1peter",
+            "1 pet",
+            "1st pet",
+            "1st peter",
+            "first peter"
+        ]
+    },
+    {
+        name: "2 Peter",
+        aliases: [
+            "2pe",
+            "2peter",
+            "2 pet",
+            "2nd pet",
+            "2nd peter",
+            "second peter"
+        ]
+    },
+    {
+        name: "1 John",
+        aliases: [
+            "1jn",
+            "1john",
+            "1st john",
+            "first john"
+        ]
+    },
+    {
+        name: "2 John",
+        aliases: [
+            "2jn",
+            "2john",
+            "2nd john",
+            "second john"
+        ]
+    },
+    {
+        name: "3 John",
+        aliases: [
+            "3jn",
+            "3john",
+            "3rd john",
+            "third john"
+        ]
+    },
     { name: "Jude", aliases: ["jud"] },
     { name: "Revelation", aliases: ["rev"] },
 ];
