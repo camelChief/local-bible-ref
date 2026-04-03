75bd2ec8cfc4401d15f5fed5e2ffc20e7ae0fdde
Caleb Campbell <ccampbell@biscit.com.au>
2026-04-03 13:56:36 +1100

    22 - Added Czech language support.

---
+++ src/i18n/cs/common-labels.ts
@@ -0,0 +1,7 @@
+import { CommonLabels } from '../models';
+
+export const COMMON_LABELS: CommonLabels = {
+	folderDoesNotExist: 'Složka v cestě neexistuje:',
+	settingsNotConfigured:
+		'Nastavení lokálních biblických odkazů není nakonfigurováno. Před pokusem o odkazování na pasáže prosím nastavte cestu k biblickým textům.',
+};
